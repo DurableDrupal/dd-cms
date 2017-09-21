6e5b451c9d64ceac4d086c5e4af19e0af2188934
victorkane <victorkane@gmail.com>
2017-09-21 19:07:00 -0400

    markdown front matter data sent to SCS as json

--- upsert/upsert.js
+++ upsert/upsert.js
@@ -1,11 +1,10 @@
 var fs = require('fs')
 var matter = require('gray-matter')
 var config = require('../config')
+var request = require('request')
 
 var host = config.server.host;
 var port = config.server.port;
-
-console.log(host, port)
 
 file = '../content/textos/adipiscing-gansta.md'
 
@@ -17,7 +16,23 @@ fs.readFile(file, 'utf8', function (err,data) {
   // parse front matter
   text = matter(data)
   console.log(text);
-
   // upsert (based on unique slug) all slugs could have timestamp, etc.
-
+  request(
+    {
+      method: 'PUT',
+      json: text,
+      headers: {'Content-Type': 'application/json'},
+      url: 'http://' + host + ':' + port + '/api/texts' 
+    },
+    function (error, response) {
+      /*
+      if (response.statusCode== 201) {
+        console.log(201)
+      } else {
+        console.log('error ' + response.statusCode)
+      }
+      */
+      console.log('got it')
+    }
+  )
 })
